c868ebbd54b978b1a296e42db564b0db80dcb4a1
Mohsen Esmaeili <m-esm@hotmail.com>
2019-12-05 00:46:26 +0330

    init

---
+++ src/start.ts
@@ -0,0 +1,12 @@
+import MultiBranch from ".";
+import wrapLogs from "./utils/wrap-logs";
+
+wrapLogs();
+
+MultiBranch.bootstrap()
+  .then(() => {
+    console.info("Multi branch started !");
+  })
+  .catch(e => {
+    console.error("Error in MultiBranch bootstrap process !", e);
+  });
